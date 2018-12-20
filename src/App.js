65ea1fd47ebe00cdd42c3e50ec0c760426e21556
tunikov <tunikov.spb@gmail.com>
2018-12-20 15:13:51 +0300

    linter fixes, added prop types

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ class App extends Component {
       winner: false
     }
   }
-  setBoxerPosition(boxer,leftPosition){
-    this.setState({allowMoving: true})
-    const limitMoving = () => this.setState({allowMoving: false})
+  setBoxerPosition(boxer, leftPosition){
+    this.setState({ allowMoving: true })
+    const limitMoving = () => this.setState({ allowMoving: false })
 
     if(boxer == 'rightBoxer'){
       if(leftPosition >= 750 || leftPosition <= (this.state.leftBoxerPosition + 50)) return limitMoving()
@@ -29,54 +29,54 @@ class App extends Component {
     if(boxer == 'leftBoxer'){
       if(leftPosition <= 150 || leftPosition >= (this.state.rightBoxerPosition - 50)) return limitMoving()
     }
-    this.setState({[`${boxer}Position`]: leftPosition})
+    this.setState({ [`${boxer}Position`]: leftPosition })
   }
-  toDamage(boxer,leftPosition,value){
+  toDamage(boxer, leftPosition) {
     if(boxer == 'rightBoxer' && leftPosition <= (this.state.leftBoxerPosition + 55)){
-      this.setState({leftBoxerHealth: this.state.leftBoxerHealth - 20})
+      this.setState({ leftBoxerHealth: this.state.leftBoxerHealth - 20 })
     }
     if(boxer == 'leftBoxer' && leftPosition >= (this.state.rightBoxerPosition - 55)){
-      this.setState({rightBoxerHealth: this.state.rightBoxerHealth - 20})
+      this.setState({ rightBoxerHealth: this.state.rightBoxerHealth - 20 })
     }
     if(this.state.rightBoxerHealth <= 0 || this.state.leftBoxerHealth <= 0){
-      this.setState({stopGame: true})
-      this.setState({winner: this.state.rightBoxerHealth <= 0 ? 'Left Boxer' : 'Right Boxer'})
+      this.setState({ stopGame: true })
+      this.setState({ winner: this.state.rightBoxerHealth <= 0 ? 'Left Boxer' : 'Right Boxer' })
     }
   }
-  setJabDamage(boxer,leftPosition){
-    this.toDamage(boxer,leftPosition,5)
+  setJabDamage(boxer, leftPosition){
+    this.toDamage(boxer, leftPosition, 5)
   }
-  setHookDamage(boxer,leftPosition){
-    this.toDamage(boxer,leftPosition,20)
+  setHookDamage(boxer, leftPosition){
+    this.toDamage(boxer, leftPosition, 20)
   }
   render(){
     return (
       <div className="wrapper">
         <div className="ring">
-          <div className="ring-inner"></div> 
-          <Score name="left" health={this.state.leftBoxerHealth} />
-          <Score name="right" health={this.state.rightBoxerHealth} />
+          <div className="ring-inner" />
+          <Score health={this.state.leftBoxerHealth}
+            name="left" />
+          <Score health={this.state.rightBoxerHealth}
+            name="right" />
           <div className="speaker">{this.state.winner ? `${this.state.winner} wins` : ''}</div>
-          <Boxer 
-            classText="leftBoxer" 
+          <Boxer
+            allowMoving={this.state.allowMoving}
+            classText="leftBoxer"
             leftPosition={this.state.leftBoxerPosition}
-            setPosition={this.setBoxerPosition}
             opponentPosition={this.state.rightBoxerPosition}
-            setJabDamage={this.setJabDamage} 
             setHookDamage={this.setHookDamage}
-            allowMoving={this.state.allowMoving}
-            stopGame={this.state.stopGame}
-          />
-          <Boxer 
-            classText="rightBoxer" 
-            leftPosition={this.state.rightBoxerPosition}
+            setJabDamage={this.setJabDamage}
             setPosition={this.setBoxerPosition}
-            opponentPosition={this.state.leftBoxerPosition}
-            setJabDamage={this.setJabDamage} 
-            setHookDamage={this.setHookDamage}
+            stopGame={this.state.stopGame} />
+          <Boxer
             allowMoving={this.state.allowMoving}
-            stopGame={this.state.stopGame}
-          />
+            classText="rightBoxer"
+            leftPosition={this.state.rightBoxerPosition}
+            opponentPosition={this.state.leftBoxerPosition}
+            setHookDamage={this.setHookDamage}
+            setJabDamage={this.setJabDamage}
+            setPosition={this.setBoxerPosition}
+            stopGame={this.state.stopGame} />
         </div>
       </div>
     )
